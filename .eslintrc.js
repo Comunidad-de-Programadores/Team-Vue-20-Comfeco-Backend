5b622359fd3ce4f5d397a5de859ae4eebbf99a0c
Raul Novelo <rnovelo.cruz98@gmail.com>
2021-02-13 18:00:23 -0600

    fix: eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,16 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'es2021': true,
-		'node': true
-	},
-	'extends': 'eslint:recommended',
-	'parserOptions': {
-		'ecmaVersion': 12,
-		'sourceType': 'module'
-	},
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'windows'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		]
-	}
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["prettier", "eslint:recommended"],
+  parser: "babel-eslint",
+  plugins: ["prettier"],
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "windows"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
 };
